f4a0bbd20313100396b59f033b4e0b15e89da71f
bartek 'basz' szurgot <git@baszerr.eu>
2022-08-07 12:30:24 +0200

    switched default F_CPU to 9.6MHz (default for internal RC oscillator)

--- sw/config.hpp
+++ sw/config.hpp
@@ -3,5 +3,6 @@
 #include <avr/io.h>
 
 constexpr uint32_t kHz=1000L;
-constexpr uint32_t fcpu = (128*kHz)/8;
+constexpr uint32_t MHz=1000L * kHz;
+constexpr uint32_t fcpu = (9*MHz + 600*kHz)/8;
 #define F_CPU fcpu
